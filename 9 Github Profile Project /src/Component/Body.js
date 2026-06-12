0ef95f31a8c44a53f4d63d47e641c573466c7373
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-12 11:05:33 +0530

    commit it

---
+++ 9 Github Profile Project /src/Component/Body.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+function Body() {
+  const [Profile, setProfile] = useState([]);
+
+  const [numberOfProfile, setnumberOfProfile] = useState("");
+
+  async function generateProfile(count) {
+    const rand = Math.floor(Math.random() * 9999 - 1000 + 1) + 1000;
+    const response = await fetch(`https://api.github.com/users?since=${rand}&per_page=${count}`);
+    const data = await response.json();
+    setProfile(data);
+  }
+  useEffect(() => {
+    generateProfile(10);
+  }, [])
+  
+
+  return (
+    <>
+      <div className="but">
+        <input type="text" className="input" placeholder="Search no of Profiles" value={numberOfProfile} onChange={(e) => setnumberOfProfile(e.target.value)}></input>
+        <button onClick={() => generateProfile(Number(numberOfProfile))}>Search</button>
+      </div>
+      <div className="profiles">
+        {
+          Profile.map((value) => {
+            return (<div key={value.id} className="cards">
+              <img src={value.avatar_url}></img>
+              <h2>{value.login}</h2>
+              <a href={value.html_url} target="_blank">Profile</a>
+            </div>
+            )
+          })
+        }
+      </div>
+    </>
+  )
+}
+
+export default Body;
---
+++ 9 Github Profile Project /src/Component/Header.js
@@ -0,0 +1,12 @@
+
+function Header(){
+    return(
+        <>
+          <div id="heading">
+            <h1>Profile of Github User</h1>
+          </div>
+        </>
+    )
+}
+
+export default Header;
