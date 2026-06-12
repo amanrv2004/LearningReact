938faeff0017d6a2d0e77b04d047481437883c10
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-12 08:06:14 +0530

    new commit

---
+++ 8 Password generator in React and Closure/src/main.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+function PasswordGenerator(){
+    
+    const [Password , setPassword] = useState("");
+    const [length, setLength] = useState(12);
+    const [numberChanged ,setnumberChanges]  = useState(false);
+    const [charChanged ,setcharChanges]  = useState(false);
+    
+    function generatePassword(){
+        let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(numberChanged){
+            str += "0123456789";
+        }
+        if(charChanged){
+           str += "@#$%^&*()!<>{}[]?~+-=-";
+        }
+        let pass = "";
+        for(let i =0; i < length ;i++){
+            pass += str[Math.floor(Math.random()*str.length)]
+        }
+        setPassword(pass);
+    };
+    
+    useEffect(()=>{
+        generatePassword();
+    },[length,numberChanged,charChanged]) 
+
+    return (
+        <>
+          <h1>{Password}</h1>
+          <div className="second">
+            <input type="range" min={5} max={20} value={length} onChange={(e)=>setLength(e.target.value)}></input>
+            <label>Length is : {length}</label>
+            <input type="checkbox" defaultChecked={numberChanged} onChange={()=>setnumberChanges(!numberChanged)}></input>
+            <label>Number</label>
+            <input type="checkbox" defaultChecked={charChanged} onChange={()=>setcharChanges(!charChanged)}></input>
+            <label>Character</label>
+          </div>
+        </>
+    )
+}
+
+
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<PasswordGenerator/>)
+
+
