f0f7ad10cc44d299218baaf807b6dbe31650f94c
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-13 12:01:54 +0530

    link

--- 12 React Router/app.js
+++ 12 React Router/app.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useMemo, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter,Route, Routes} from "react-router";
+import { BrowserRouter,Route, Routes,Link} from "react-router";
 import Home from "./src/Home";
 import Dashboard from "./src/Dashboard";
 import Contact from "./src/Contact";
@@ -10,6 +10,11 @@ function App(){
     return(
            <>
                <BrowserRouter>
+               <nav>
+                 <Link to="/">Home</Link>
+                 <Link to="/Contact">Contact</Link>
+                 <Link to="/Dashboard">Dashboard</Link>
+               </nav>
                     <Routes>
                         <Route path="/" element={<Home/>}></Route>
                         <Route path="/Contact" element={<Contact/>}></Route>
