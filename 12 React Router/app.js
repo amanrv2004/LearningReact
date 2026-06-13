4bca7144aeff80cea75b0ab2a00dae8e950e4ea8
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-13 11:56:26 +0530

    Routes

--- 12 React Router/app.js
+++ 12 React Router/app.js
@@ -1,16 +1,24 @@
 import React, { useState, useCallback, useMemo, useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter,Route, Routes} from "react-router";
+import Home from "./src/Home";
+import Dashboard from "./src/Dashboard";
+import Contact from "./src/Contact";
 
 function App(){
 
-    const [count,setCount] = useState(0)
-
     return(
-        <>
-            <h1>Count is {count}</h1>
-            <button onClick={()=>setCount(count+1)}>Increment</button>
-        </>
-    )
+           <>
+               <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}></Route>
+                        <Route path="/Contact" element={<Contact/>}></Route>
+                        <Route path="/Dashboard" element={<Dashboard/>}></Route>
+                    </Routes>
+               </BrowserRouter>
+
+           </> 
+        )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
