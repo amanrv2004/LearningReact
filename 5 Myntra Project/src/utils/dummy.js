a28ce724c852b44e10a374399fadd925b2ab2273
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-15 12:04:08 +0530

    change

--- 5 Myntra Project/src/utils/dummy.js
+++ 5 Myntra Project/src/utils/dummy.js
@@ -1,11 +1,11 @@
 const arr = [{cloth:"Tshirt", Offer:"20-40%Off",price:200},{cloth:"Pant", Offer:"30-50%Off" ,price:500},{cloth:"Skirt", Offer:"10-20%Off",price:250},{cloth:"Kurta", Offer:"30-60%Off",price:300},{cloth:"Patloon", Offer:"11-40%Off",price:550},{cloth:"Shoes", Offer:"40-60%Off",price:800},{cloth:"Shirt", Offer:"10-20%Off",price:499},{cloth:"Bag", Offer:"15-40%Off",price:600}]
 
-export function greet(){
-    return <h1>Hello Coder Army</h1>
-}
+// export function greet(){
+//     return <h1>Hello Coder Army</h1>
+// }
 
-export function meet(){
-    return <h1>Hello Bhaiya ji</h1>
-}
+// export function meet(){
+//     return <h1>Hello Bhaiya ji</h1>
+// }
 
 export default arr;
