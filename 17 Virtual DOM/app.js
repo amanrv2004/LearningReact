b758a2ed82f83ab1a47e0838685a03a00f7b34ab
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-17 11:14:49 +0530

    1

--- 17 Virtual DOM/app.js
+++ 17 Virtual DOM/app.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDOM from 'react-dom/client';
 import Add from "./Add";
 const arr = [1,2,3,4,2,5]
 
 function App(){
     
+//    const Vdom = <h1>Hello</h1>;
+//    const RDom = createElement('h1');
+//    RDom.innerText = "Aman";
+//    console.log(Vdom);
+//    console.dir(RDom);
+
+
+
+
     return (
         <>
            {
