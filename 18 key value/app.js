c86a92ef87e83271e2b0a7f73c32a471377c227c
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-17 10:36:20 +0530

    1

---
+++ 18 key value/app.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ReactDOM from 'react-dom/client';
+import Add from "./Add";
+
+
+function App(){
+
+
+const [language,setLanguage] = useState(['TS','JS','JAVA'])
+    
+    return (
+
+        <>
+        </>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
