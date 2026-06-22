06a8ab99b87907fe2106ec3905d85d7750ce5d2d
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 10:22:29 +0530

    3

--- 20 P1/src/component/Header.js
+++ 20 P1/src/component/Header.js
@@ -3,13 +3,13 @@ export default function Header() {
     <header className="bg-[#ff5200]">
       <div className="flex justify-between container mx-auto py-8">
                 <img className="w-40 h-12" src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png"></img>
-                <div>
+                <div className="font-serif text-white text-base font-bold flex gap-15 items-center">
                     <a target="_blank" href="https://www.swiggy.com/corporate/">Swiggy Corporate</a>
                     <a target="_blank" href="https://partner.swiggy.com/login#/swiggy">Partner with Us</a>
                     <a className="border border-white py-3 px-4 rounded-2xl" target="_blank" href="https://www.swiggy.com/corporate/">Get The App</a>
-                    <a className="border border-black bg-black py-3 px-4 rounded-2xl"target="_blank" href="https://www.swiggy.com/corporate/">Sign in</a>
+                    <a className="border border-black bg-black py-3 px-3 rounded-2xl" target="_blank" href="https://www.swiggy.com/corporate/">Sign in</a>
                 </div>
-            </div>
+        </div>
     </header>
   );
 }
