132886db08d4689d5b26f34a17de03570a4a665b
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 13:18:10 +0530

    14

--- 21 P1/src/component/DineCard.js
+++ 21 P1/src/component/DineCard.js
@@ -3,7 +3,7 @@
 export default function DineCard({ RestData }) {
 
     return (
-        <div className="max-w-sm">
+        <div className="max-w-sm flex-none">
             <div>
                 <img src={"https://media-assets.swiggy.com/swiggy/image/upload/" +
                     RestData?.info?.mediaFiles[0]?.url}></img>
