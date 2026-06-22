c6b29c98d560b180d7c81a050ad0cb2ac4c384e8
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 12:12:31 +0530

    10

--- 20 P1/src/component/GroceryOption.js
+++ 20 P1/src/component/GroceryOption.js
@@ -1,19 +1,20 @@
-import {GroceryGridCards} from "../utils/Grocery";
+import { GroceryGridCards } from "../utils/Grocery";
 import GroceryCard from "./GroceryCard";
 
 
-export default function GroceryOption(){
+export default function GroceryOption() {
 
-    return(
-        <>
-        <div className="mt-20 w-[80%] container mx-auto mb-10">
-            <h1>Shop groceries on Instamart</h1>
-            <div className="w-[80%] mx-auto px-4 grid grid-flow-col auto-cols-max overflow-x-auto gap-5">
-                {
-                    GroceryGridCards.map((foodData)=><GroceryCard  key={foodData.id} foodData = {foodData}/>)
-                }
+    return (
+            <div className="mt-20 w-[80%] mx-auto mb-10">
+                <h1 className="mb-4">Shop groceries on Instamart</h1>
+
+                <div className="px-4">
+                    <div className="grid grid-flow-col auto-cols-max overflow-x-auto gap-3  hide-scrollbar">
+                        {GroceryGridCards.map((foodData) => (
+                            <GroceryCard key={foodData.id} foodData={foodData} />
+                        ))}
+                    </div>
+                </div>
             </div>
-        </div>
-        </>
     )
 };
