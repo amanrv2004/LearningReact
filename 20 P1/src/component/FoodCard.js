132af7fbd65467a3de55274dd3059210c3def631
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 11:37:07 +0530

    6

--- 20 P1/src/component/FoodCard.js
+++ 20 P1/src/component/FoodCard.js
@@ -3,7 +3,7 @@
 export default function FoodCard({foodData}){
     return(
         <>
-           <a href={""}>
+           <a href={foodData?.action?.link}>
              <img className="h-50 w-40 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/" + foodData?.imageId}></img>
            </a>
         </>
