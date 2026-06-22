a26a63b5ab0210928a3ae18f66db1e878864e778
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 14:07:39 +0530

    18

--- 21 P1/src/App.js
+++ 21 P1/src/App.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./component/Header";
 import FoodOption from "./component/FoodOption";
 import GroceryOption from "./component/GroceryOption";
 import DineOption from "./component/DineOption";
-function App() {
-  return (
-    <>
-      <Header></Header>
-      <FoodOption></FoodOption>
-      <GroceryOption></GroceryOption>
-      <DineOption/>
-    </>
-  );
+import Restaurant from "./component/Restaurant";
+
+
+
+// Header section: Let's build it
+
+function App(){
+    
+    return(
+       <>
+       <Header></Header>
+       <FoodOption></FoodOption>
+       <GroceryOption></GroceryOption>
+       <DineOption></DineOption>
+       <Restaurant></Restaurant>
+       </>
+    )
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
+
+
+
+// Proxy server "https://cors-anywhere.herokuapp.com/"; 
+
