3d11ad00655082a82cde47af918ad85dd8febdc7
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 15:58:21 +0530

    20

--- 21 P1/src/component/Restaurant.js
+++ 21 P1/src/component/Restaurant.js
@@ -9,7 +9,7 @@ export default function Restaurant(){
     
      async function fetchData() {
         
-        const proxyServer = "https://cors-anywhere.herokuapp.com/"
+        const proxyServer = "https://corsproxy.io/?"
         const swiggyAPI = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true";
         const response = await fetch(proxyServer+swiggyAPI);
         const data = await response.json();
