a27da8c5c3ca6335b489ff74a5c6250b0a2c67f9
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-22 09:49:51 +0530

    1

---
+++ 20 P1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+function App(){
+
+    return (
+        <>
+            <h1>Hello Aman</h1>
+        </>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
