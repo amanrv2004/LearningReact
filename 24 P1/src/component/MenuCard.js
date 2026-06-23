71c26d814220a4c5015f844cc4b4feb49fba8052
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-23 18:09:09 +0530

    23

--- 24 P1/src/component/MenuCard.js
+++ 24 P1/src/component/MenuCard.js
@@ -2,46 +2,57 @@ import { useState } from "react"
 import RestInfo from "./RestInfo"
 
 
-export default function MenuCard({menuItems}){
+export default function MenuCard({ menuItems }) {
 
     const [isOpen, setIsOpen] = useState(true);
-    
-    if("categories" in menuItems){
-        return(
+
+    const [selected, setSelected] = useState(null)
+
+    if ("categories" in menuItems) {
+        return (
             <div className="w-full">
-            <p className="text-2xl font-bold">{menuItems.title}</p>
-            <div>
-                {
-                    menuItems?.categories?.map((items)=> <MenuCard key={items?.title} menuItems={items}></MenuCard>)
-                }
+                <p className="text-2xl font-bold">{menuItems.title}</p>
+                <div>
+                    {
+                        menuItems?.categories?.map((items) => <MenuCard key={items?.title} menuItems={items}></MenuCard>)
+                    }
+                </div>
             </div>
+        )
+    }
+    
+
+
+
+
+    if (!isOpen) {
+        return (
+            <div>
+                <div className="w-[80%] mt-20 mb-20">
+                    <button className="text-2xl py-2 px-8 mr-2 border rounded-2xl " onClick={()=>setSelected(selected ==='veg'?null:'veg')}>Veg</button>
+                    <button className="text-2xl py-2 px-4 mr-2 border rounded-2xl " onClick={()=>setSelected(selected ==='nonveg'?null:'nonveg')}>Non Veg</button>
+                </div>
+                <div className="w-full">
+                    <div className="flex justify-between w-full">
+                        <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
+                        <button className="text-5xl font-bold mr-20" onClick={() => setIsOpen(!isOpen)}>{isOpen ? '^' : '⌄'}</button>
+                    </div>
+                    <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
+                </div>
             </div>
         )
     }
 
-   
-    if(!isOpen){
-        return(
-        <div className="w-full">
-            <div className="flex justify-between w-full">
-            <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
-            <button className="text-5xl font-bold mr-20" onClick={()=>setIsOpen(!isOpen)}>{isOpen?'^':'⌄'}</button>
-            </div>
-            <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
-        </div>   
-        ) 
-    }
-    
 
     return (
         <div className="w-full">
             <div className="flex justify-between w-full">
-            <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
-            <button className="text-5xl font-bold mr-20" onClick={()=>setIsOpen(!isOpen)}>{isOpen?'^':'˯'}</button>
+                <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
+                <button className="text-5xl font-bold mr-20" onClick={() => setIsOpen(!isOpen)}>{isOpen ? '^' : '˯'}</button>
             </div>
             <div>
                 {
-                    menuItems?.itemCards?.map((items)=><RestInfo key={items?.card?.info?.id} restData = {items?.card?.info}></RestInfo>)
+                    menuItems?.itemCards?.map((items) => <RestInfo key={items?.card?.info?.id} restData={items?.card?.info}></RestInfo>)
                 }
             </div>
             <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
