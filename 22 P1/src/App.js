c16609c17dc5f2b00373cd807eb4049e7bc5680d
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-23 08:21:48 +0530

    5

--- 22 P1/src/App.js
+++ 22 P1/src/App.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./component/Home";
 import Restaurant from "./component/Restaurant";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 // Header section: Let's build it
 
-function App(){
-    
-    return(
-       <>
-       <Home/>
-       <Restaurant></Restaurant>
-       </>
+function App() {
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes >
+                        <Route path="/" element={<Home/>}></Route>
+                        <Route path="/restaurant" element={<Restaurant/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
@@ -21,5 +25,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
 
 
 
-// Proxy server "https://cors-anywhere.herokuapp.com/"; 
+// Proxy server "https://corsproxy.io/?"; 
 
