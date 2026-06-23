53c2952c32d810fe29c296f82947f5a81e22dea3
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-23 11:18:35 +0530

    17

--- 23 P1/src/component/RestInfo.js
+++ 23 P1/src/component/RestInfo.js
@@ -1,7 +1,22 @@
 
 
-export default function RestInfo(){
+export default function RestInfo({restData}){
     return (
-        <h1>AMan</h1>
+        <>
+            <div className="flex w-full justify-between">
+                <div className="w-[70%]">
+                        <p>{restData?.name}</p>
+                        <p>{"$"+restData?.defaultPrice/100}</p>
+                        <span>{restData?.ratings?.aggregatedRating?.rating}</span>
+                        <span>{"("+restData?.ratings?.aggregatedRating?.ratingCountV2+")"}</span>
+                        <p>{restData?.description}</p>
+                        
+                </div>
+                <div className="w-[20%]">
+
+                </div>
+
+            </div>
+        </>
     )
 }
