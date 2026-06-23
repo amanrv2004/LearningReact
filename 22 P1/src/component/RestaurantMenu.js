1ba65aecba2dded0a651ca60f74ef808d541a5a5
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-23 10:01:37 +0530

    12

--- 22 P1/src/component/RestaurantMenu.js
+++ 22 P1/src/component/RestaurantMenu.js
@@ -1,13 +1,10 @@
 
 import { useEffect ,useState } from "react";
 import { useParams } from "react-router-dom"
-
 export default function RestaurantMenu(){
-    
     let {id}= useParams();
     const [RestData, setRestData] = useState(null);
     useEffect(()=>{
-    
         async function fetchData() {
            const proxyServer = "https://cors-anywhere.herokuapp.com/"
            const swiggyAPI = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
@@ -15,13 +12,9 @@ export default function RestaurantMenu(){
            const data = await response.json();
            setRestData(data);
         }
-   
         fetchData();
        },[])
-
-
     return (
-
         <>
           <div>{id}</div>
         </>
