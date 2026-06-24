721250fe58fbdfa78912ef5ff19fce6a44b56eba
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-24 10:54:37 +0530

    8

--- 26 P1/src/Components/Header.js
+++ 26 P1/src/Components/Header.js
@@ -36,13 +36,6 @@ export default function Header(){
                 <a href="https://www.swiggy.com/instamart?entryId=1234&entryName=mainTileEntry4&v=1">
                     <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/16/ca34e375-f1bd-4a2e-a3e7-0a20833be83b_IM4BU1.png"></img>
                 </a>
-                <a href="https://www.swiggy.com/dineout">
-                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/1/76c30e5a-8adb-4795-bf5b-fa64e9e9e1d3_DO4BU.png"></img>
-                </a>
-                <a href="https://www.swiggy.com/genie">
-                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/31/14033c0b-8907-420b-b72a-d26cfa68dc7b_Genie4BU.png"></img>
-                </a>
-
             </div>
         </header>
     )
