0d7a2234acece79643c4b456d8023496dcdce26e
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-24 09:43:06 +0530

    2

---
+++ 25 P1/src/component/RestHeader.js
@@ -0,0 +1,16 @@
+
+
+export default function RestHeader(){
+
+
+    return (
+        <div className="conatiner mx-auto py-4 px-8 bg-gray-200 text-5xl flex justify-between item-center">
+            <div>
+                    <p className="bg-orange-600">Swiggy</p>
+            </div>
+            <div>
+                    <p>Cart</p>
+            </div>
+        </div>
+    )
+}
